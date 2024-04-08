1c4a5d96917897622a952ec9a3aeaf192c711e07
vk2098 <kapavivek98@gmail.com>
2024-04-07 21:35:45 -0500

    Added Load balancing to the Question Service and changed openfeign in quiz

--- QuizService/src/main/java/com/quiz/QuizService/service/QuestionClient.java
+++ QuizService/src/main/java/com/quiz/QuizService/service/QuestionClient.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8082",name = "Question-Client")
+//@FeignClient(url = "http://localhost:8082",value = "Question-Client")
+
+@FeignClient(name="QUESTION-SERVICE")
 public interface QuestionClient {
 
 
